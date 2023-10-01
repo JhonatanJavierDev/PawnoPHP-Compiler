174da369dffca1db6b10c536320ac38f79d13e52
sneedman1337 <lewawi422@gmail.com>
2023-09-30 21:13:58 -0400

    pathlib file handling

--- compiler.py
+++ compiler.py
@@ -1,8 +1,7 @@
 import subprocess
-import os
 import uuid
 import re
-import glob
+from pathlib import Path
 
 class CodeCompiler:
     def __init__(self):
@@ -13,19 +12,21 @@ class CodeCompiler:
             'stack_size': r'Stack/heap size:\s*(\d+)',
             'total_requirements': r'Total requirements:\s*(\d+)',
         }
-
+    
     def compile_code(self, code):
         try:
             compiler_path = 'pawncc/pawncc'
             file_name = str(uuid.uuid4()) + '.pwn'
-            file_path = os.path.join('projects', file_name)
-            os.makedirs('projects', exist_ok=True)
+            project_dir = Path('projects')
+            file_path = project_dir / file_name
 
-            with open(file_path, 'w') as f:
-                f.write('#pragma option -d3\n\n')
+            project_dir.mkdir(parents=True, exist_ok=True)
+
+            with file_path.open('w') as f:
+                f.write('#pragma option -d3\n\n') # bullshit
                 f.write(code)
             
-            output = subprocess.check_output([compiler_path, file_path], stderr=subprocess.STDOUT, universal_newlines=True)
+            output = subprocess.check_output([compiler_path, str(file_path)], stderr=subprocess.STDOUT, universal_newlines=True)
 
             self.delete_amx()
 
@@ -38,17 +39,16 @@ class CodeCompiler:
             return result
         except subprocess.CalledProcessError as e:
             return {'success': False, 'complete_output': e.output}
-        
+    
     def delete_amx(self):
         deleted_amx_files = []
-        for amx_file in glob.glob('*.amx'):
-            if not os.path.join(os.getcwd(), amx_file).startswith(os.path.join(os.getcwd(), 'projects')):
-                try:
-                    os.remove(amx_file)
-                    deleted_amx_files.append(amx_file)
-                except OSError as e:
-                    print(f'Error deleting {amx_file}: {e}')
-                else:
-                    print(f'{amx_file} deleted')
+        
+        for amx_file in Path('.').glob('*.amx'):
+            try:
+                amx_file.unlink()
+                deleted_amx_files.append(amx_file)
+                print(f'{amx_file} deleted')
+            except OSError as e:
+                print(f'Error deleting {amx_file}: {e}')
 
         return deleted_amx_files
