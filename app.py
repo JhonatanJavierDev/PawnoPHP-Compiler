1811e0787ef1acce9210bc07eb8345e8b017b217
skypper <126270596+xypp1337@users.noreply.github.com>
2023-10-03 07:03:21 -0400

    waszaasa

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 compiler = CodeCompiler()
 
 @app.route('/compile', methods=['POST'])
-def compile_code():
+def compile_endpoint():
     code = request.json.get('code')
     result = compiler.compile_code(code)
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=1337)
